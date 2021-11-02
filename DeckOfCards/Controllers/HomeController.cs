dcf864ea1ba0f1a88701309a2aeb8e3befab5eb3
Yousif <ybeeai@gmail.com>
2021-11-02 16:58:08 -0400

    YB commit

---
+++ DeckOfCards/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using DeckOfCards.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private CardDAL cardDAL = new CardDAL();
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CardResult()
+        {
+            CardModel result = cardDAL.GetData();
+            return View(result);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCards/Models/CardDAL.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace DeckOfCards.Models
+{
+    public class CardDAL
+    {
+        public CardModel GetData()
+        {
+            string key = "huvcludux6us";
+            string url = $"https://deckofcardsapi.com/api/deck/{key}/draw/?count=5";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+
+            string JSON = reader.ReadToEnd();
+
+            CardModel result = JsonConvert.DeserializeObject<CardModel>(JSON);
+
+            return result;
+        }
+    }
+}
